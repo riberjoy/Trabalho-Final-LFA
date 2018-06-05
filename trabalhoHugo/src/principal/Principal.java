daf06d5e099cbd200906d27edcc97713c2dfcdd5
JoyceR <riber.joy@gmail.com>
2018-06-05 16:52:27 -0300

    Olhar AlgoritonosGrafos

--- trabalhoHugo/src/principal/Principal.java
+++ trabalhoHugo/src/principal/Principal.java
@@ -3,19 +3,18 @@ package principal;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import javax.swing.JOptionPane;
 
 public class Principal {
     
     static  File f ;
 
-    public static void setFile(File f) throws FileNotFoundException {
-        
+    public Principal(File f) throws FileNotFoundException {
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         Scanner sc = new Scanner(f);
-        while (sc.hasNextLine()){
-            //System.out.println(sc.nextLine());
-            JOptionPane.showMessageDialog(null, "deu");
+        while (sc.hasNextLine()== true){
+            System.out.println(sc.nextLine());
+            //byte nome = sc.nextByte();
+            //JOptionPane.showMessageDialog(null, nome);
         }
     }
 }
